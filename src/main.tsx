6248bf8165f26ab5870a7661528c0dfee8dbe7db
JENkt4k <james.e.nelson@gmail.com>
2025-08-27 08:34:45 -0500

    fixed styles rename issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 
 createRoot(document.getElementById('root')!).render(<App />);
 
-// Register service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(console.error);
