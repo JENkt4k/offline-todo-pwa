1e3eabf3c37e7fe78add851874ec3f32fc247a0c
JENkt4k <james.e.nelson@gmail.com>
2025-08-28 11:28:59 -0500

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH,
   plugins: [
     react(),
     VitePWA({
